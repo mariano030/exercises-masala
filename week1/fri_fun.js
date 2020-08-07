9f17fb8eced3ee5e2088c6edc62bb27df9205924
mariano030 <marian.heidbreder@gmail.com>
2020-08-07 13:44:42 +0200

    this was so much fun

---
+++ week1/fri_fun.js
@@ -0,0 +1,27 @@
+var x = 500;
+
+var doubleX = 0;
+
+function timesTwo(num) {
+    return num * 2;
+}
+
+doubleX = timesTwo(x);
+
+console.log(doubleX);
+
+var numbers;
+
+numbers = [x, doubleX];
+
+console.log(numbers);
+
+numbers.forEach(function (i) {
+    console.log(i);
+});
+
+numbers = {};
+
+numbers.y = doubleX;
+
+console.log(numbers.y);
