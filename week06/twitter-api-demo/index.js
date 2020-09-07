c121cd64337d6348e141fdc4c26808162d83ff44
mariano030 <marian.heidbreder@gmail.com>
2020-09-07 15:33:17 +0200

    twitter wip

--- week06/twitter-api-demo/index.js
+++ week06/twitter-api-demo/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const { getToken, getTweets, filterTweets } = require("./twitter.js"); // with file you can leave out extension
 
-app.use(express.static("./ticker_jquery_ajax_json"));
+app.use(express.static("./ticker"));
 
 // this route is to fake a  headlines.json that would be requested here ( by script.js gb)
 app.get("/headlines.json", (req, res) => {
@@ -16,21 +16,23 @@ app.get("/headlines.json", (req, res) => {
             return;
         }
         getTweets(bearerToken, (err, tweets) => {
+            console.log("where are my tweets");
             if (err) {
                 console.log("err in getTweets", err);
                 return;
             }
-
             // do stuff with the tweets you have
         });
-        cxonsole.log("tweets in index.js!!", tweets);
-        const filteredTweets = filterTweets(tweets);
+        console.log("tweets in index.js!!", " i dont have the tweets yet");
+        //const filteredTweets = filterTweets(tweets);
+        console.log("can't filter what I don't have: tweets");
         // array of objects is needed format!
         // 2. with the token make a request for tweets
         // 3. once we receive the tweets, filter them
         // 4. send filtered tweets to the client as json
 
-        res.json(filteredTweets);
+        //res.json(filteredTweets);
+        res.end();
     });
 });
 
--- week06/twitter-api-demo/twitter.js
+++ week06/twitter-api-demo/twitter.js
@@ -1,11 +1,8 @@
 const https = require("https");
-const { consumerKey, consumerSecret } = require("./secrets"); // destructuring
+const { consumerKey, consumerSecret } = require("./secrets");
 
 module.exports.getToken = function (callback) {
-    //this funciton will get the bearerToken from twitter
-    // later
-
-    let creds = `${consumerKEy}:${consumerSecrets}`;
+    let creds = `${consumerKey}:${consumerSecret}`;
     let encodedCreds = new Buffer.from(creds).toString("base64");
 
     const options = {
@@ -14,46 +11,89 @@ module.exports.getToken = function (callback) {
         method: "POST",
         headers: {
             Authorization: `Basic ${encodedCreds}`,
-            contentType: `application/x-www-form-urlencoded;
-            charset-UTF-8`,
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
         },
     };
-    // options - jobject that has info for the request
-    // cbForAfterReq a callback for after the request is finished
-    const req = https.request(options, cbForAfterReq);
+
+    // options - obj that has info about the request we're about to make
+    // cb - callback that runs WHEN we get info back from the request
+    const req = https.request(options, cb);
     req.end("grant_type=client_credentials");
 
     function cb(resp) {
-        if (resp.stawtusCode != 200) {
+        if (resp.statusCode != 200) {
+            // something went wrong!
             callback(resp.statusCode);
             return;
         }
+
+        // if we get to this point, that means everything went well!
+        let body = "";
+        resp.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        resp.on("end", () => {
+            // console.log('body in twitter.js: ', body);
+            let parsedBody = JSON.parse(body);
+            // console.log(parsedBody);
+            callback(null, parsedBody.access_token);
+        });
     }
-
-    // if we get to this point, that means everything went well!
-
-    let body = "";
-
-    resp.on("data", (chunk) => {
-        body += chunk;
-    });
-
-    resp.on("end", () => {
-        console.log("content of body", body);
-        let parsedBody = JSON.parse(body);
-        callback(null, parsedBody.access_token);
-    });
 };
 
 module.exports.getTweets = function (bearerToken, callback) {
-    // gets tweets
-    // each request NEEDS the token
-    // complete please
+    // example request url:
+    // 1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2
+
+    console.log("getTweets running");
+    const options = {
+        host: "api.twitter.com",
+        path: "/1.1/statuses/user_timeline.json",
+        method: "GET",
+        query: "screen_name=nytimes&twitterapi&tweet_mode=extended&count=2",
+        headers: {
+            Authorization: `Bearer ${bearerToken}`,
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+    };
+
+    // options - obj that has info about the request we're about to make
+    // cb - callback that runs WHEN we get info back from the request
+    const req = https.request(options, cb2);
+    req.end("grant_type=client_credentials");
+
+    function cb2(resp) {
+        if (resp.statusCode != 200) {
+            // something went wrong!
+            callback(resp.statusCode);
+            return;
+        }
+
+        // if we get to this point, that means everything went well!
+        let body = "";
+        resp.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        resp.on("end", () => {
+            // console.log('body in twitter.js: ', body);
+            let parsedBody = JSON.parse(body);
+            // console.log(parsedBody);
+            callback(null, parsedBody.access_token);
+        });
+
+        console.log(resp);
+    }
+    // this function gets tweets from Twitter's API
+    // each request we make for tweets, we HAVE to do it with the token
+    // this is for you to complete :)
+    console.log("getTweets 'done'");
 };
 
 module.exports.filterTweets = function (tweets) {
-    // filter after receival
-    // complete please
+    // once we have the tweets from Twitter, this function filters them / cleans them up!
+    // this is for you to complete :)
 };
 
 // PARSE        json -> javascript   // JSON.parse()
